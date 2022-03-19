b95541b664e39718629ae50126f438f0ec306b49
Caio Tizo <caio.tizo@gmail.com>
2022-03-19 18:47:14 +0000

    Initial Push

---
+++ caiotizolib/lib.py
@@ -0,0 +1,8 @@
+def try_me():
+    user_name = input('Hi, what is your name? ')
+    print()
+    print(f'Nice to meet you, {user_name}!!')
+    print()
+    moms_name = input("Now, tell me your mother's name and I will analyse it: ")
+    print()
+    print('Yeah, she is a bitch.. see you!')
